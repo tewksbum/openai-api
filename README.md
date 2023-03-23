f1d60701afa2f372b81e4b266f220c1e25cce21b
tewksbum <tewksbum@hotmail.com>
2023-03-23 12:05:13 -0400

    cleaned up env

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # openai-api
+pip install "openai[embeddings]"
+
+## to start:
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn main:app --reload 
+
+## to kill:
+lsof -i :8000 kill
+
+# virtual env:
+python3 -m venv openai-api
+source openai-api/bin/activate
--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import router as api_v1_router
+from fastapi.middleware.cors import CORSMiddleware
+from api.compose import router as compose_router
 
 app = FastAPI(title="My FastAPI", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_originins=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(api_v1_router, prefix="/api/v1")
+app.include_router(compose_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
