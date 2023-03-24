67c7e2686d975fe0ed9ad4e29e802cc92fe8f2ed
tewksbum <tewksbum@hotmail.com>
2023-03-23 23:05:21 -0400

    added uviqorn to main.py

--- app/main.py
+++ app/main.py
@@ -18,3 +18,6 @@ app.include_router(compose_router, prefix="/api/v1")
 @app.get("/")
 def read_root():
     return {"message": "Welcome to my FastAPI project!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
