2f17e19ed7bab67aced92d59071e9579f51bc30a
tewksbum <tewksbum@hotmail.com>
2023-03-23 23:39:19 -0400

    trying gunicorn

--- app/main.py
+++ app/main.py
@@ -13,11 +13,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+# gunicorn main:app
+
 app.include_router(compose_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to my FastAPI project!"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=4, worker_class="uvicorn.workers.UvicornWorker")
+    # gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app    
