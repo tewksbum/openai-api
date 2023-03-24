c18afc8e48b0aed71cbf573c67c286dd8e6e6082
tewksbum <tewksbum@hotmail.com>
2023-03-23 21:12:24 -0400

    dropped main.py uvicorn

--- app/main.py
+++ app/main.py
@@ -18,7 +18,3 @@ app.include_router(compose_router, prefix="/api/v1")
 @app.get("/")
 def read_root():
     return {"message": "Welcome to my FastAPI project!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
